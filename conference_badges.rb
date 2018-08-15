58fcef00cac6d0fdc81fb1df3d15cab31846c5b0
Jonathan Chan <johnkchan252@gmail.com>
2018-08-15 01:44:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |attendee|
     puts attendee
   end
-  assign_rooms(attendee).each do |attendee|
+  assign_rooms(attendees).each do |attendee|
     puts attendee
   end
 end
